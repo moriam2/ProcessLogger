09f484637bad6c73264742edee38f394699e0ce8
moriam2 <29521385+moriam2@users.noreply.github.com>
2025-05-31 23:01:14 +0100

    Prep for first nuget  release.

--- src/ProcessLogger/Options/ProcessLoggerOptions.cs
+++ src/ProcessLogger/Options/ProcessLoggerOptions.cs
@@ -2,6 +2,8 @@ using System;
 using System.Diagnostics;
 using Microsoft.Extensions.Logging;
 
+namespace ProcessLogger.Options;
+
 public class ProcessLoggerOptions
 {
     public LogLevel StartLogLevel { get; set; } = LogLevel.Information;
